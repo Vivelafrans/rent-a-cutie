e41e96d6840f9589c598bbfaf4b2223b90852046
Levin Bauschke <b.bauschke@zeppelin-university.net>
2019-08-23 12:48:42 +0200

    Added new and create in ReviewsController

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @rental = Rental.find(params[:rental_id])
+    @user = @rental.user
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @rental = Rental.find(params[:rental_id])
+    @user = @rental.user
+    @user_now = current_user
+    @review.rental = @rental
+    @review.user = @user
+    if @review.save
+      redirect_to dashboard_path(@user_now), notice: 'The review was send successfully.'
+    else
+      render :new, notice: 'Check your Review'
+    end
+end
